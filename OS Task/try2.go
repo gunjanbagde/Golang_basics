806ad0b22466a99a5ef2b183577b08840f1aae83
Gunjan Asit Bagde <Gunjan.Bagde@tanla.local>
2021-09-30 13:09:28 +0530

    OS task completed

---
+++ OS Task/try2.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/robfig/cron"
+)
+
+func main() {
+
+	// root := "C:\\go-workspace\\OS Task\\root\\"
+	// chkfiles(root)
+
+	timefunc()
+	for {
+		time.Sleep(time.Second)
+	}
+
+}
+
+func checkfiles(root string) {
+	var files []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".csv" {
+			files = append(files, path)
+			return nil
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		fmt.Println(file)
+		records := readCsvFile(file)
+		fmt.Println(records)
+	}
+}
+
+func readCsvFile(filePath string) [][]string {
+	f, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Unable to read input file "+filePath, err)
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+filePath, err)
+	}
+	return records
+}
+
+func timefunc() {
+	root := "C:\\go-workspace\\OS Task\\root\\"
+	c := cron.New()
+	c.AddFunc("@every 5s", func() {
+		log.Printf("5sec")
+		checkfiles(root)
+	})
+	c.Start()
+}
