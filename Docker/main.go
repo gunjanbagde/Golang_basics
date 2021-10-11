fc33f10b3322fd830eaa3a3187fe7b51d1f51daa
Gunjan Asit Bagde <Gunjan.Bagde@tanla.local>
2021-10-11 14:20:30 +0530

    Simple go API with docker

---
+++ Docker/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+)
+
+func main() {
+
+	fmt.Println("starting with docker")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi")
+    })
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
